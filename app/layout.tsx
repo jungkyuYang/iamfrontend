213fac14ba914c0da2a66c4589dfa1bda182e049
Jungkyu Yang <didwjd8590412@gmail.com>
2024-06-14 20:34:48 +0900

    [KAN-15] fix: import 경로 해결

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,8 @@ import { Inter } from 'next/font/google'
 
 import './styles/reset.scss'
 import './styles/global.scss'
-import Footer from './components/footer'
-import Header from './components/header'
+import Footer from './components/Footer'
+import Header from './components/Header'
 
 const inter = Inter({ subsets: ['latin'] })
 
